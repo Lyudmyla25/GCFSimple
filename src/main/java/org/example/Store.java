8d70836728124bf17cc7694796eb08605c156cc9
lyudmylaz <lyudmylaz@qualtrics.com>
2025-08-13 19:35:59 +0200

    add categories

--- src/main/java/org/example/Store.java
+++ src/main/java/org/example/Store.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class Store {
+    private String name;
+    private String address;
+
+    public Store(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
 }
